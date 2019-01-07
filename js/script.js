bebc908eaf5c4ced4f4c51ad35467dd83a5a422f
Artem Serbin <artem.serbin.77@gmail.com>
2019-01-07 22:23:20 +0300

    en

--- js/script.js
+++ js/script.js
@@ -1,15 +1,2 @@
 
-/*ЛЮБОЕ СЛОВО КОТОРОЕ НАЧИНАЕТСЯ ЧЕРЕЗ ТОЧКУ НАЗЫВАЕТСЯ МЕТОДОМ .ready
-alert("hello"); - вывводит надпись
-jQuery('document').ready(function() {
-    jQuery('body').append('<a href="#">aaaaaaayyyyyyy</a>');
-}); - вывводит ссылку на сайт*/
-/*jQuery('document').ready(function() {
-    
-    jQuery('body').append('<a href="#">aaaaaaayyyyyyy</a>');
-    
-    /*jQuery('div').remove(); - удалить все div
-    
-});*/
 
-document.getElementById('hel').innerHTML = 'hello';
